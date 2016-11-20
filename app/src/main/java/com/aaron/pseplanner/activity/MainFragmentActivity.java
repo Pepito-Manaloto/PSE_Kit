bcdc8ebb3fda26b40e8ba2f76e5f2fd1594822ba
Aaron John Asuncion <aaronjohn.asuncion@gmail.com>
2016-11-20 22:07:31 +0800

    View updates: - Added search bar. - Added refresh menu and animation. - Added setting of title on navigation item selected.

--- app/src/main/java/com/aaron/pseplanner/activity/MainFragmentActivity.java
+++ app/src/main/java/com/aaron/pseplanner/activity/MainFragmentActivity.java
@@ -2,30 +2,44 @@ package com.aaron.pseplanner.activity;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Intent;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.aaron.pseplanner.R;
+import com.aaron.pseplanner.async.UpdateTickerTask;
 import com.aaron.pseplanner.fragment.CalculatorFragment;
 import com.aaron.pseplanner.fragment.HomeFragment;
 import com.aaron.pseplanner.fragment.SettingsFragment;
 import com.aaron.pseplanner.fragment.TickerFragment;
+
+import java.lang.reflect.Field;
 
 /**
  * Abstract super class that creates a single fragment in the fragment container.
  */
 public abstract class MainFragmentActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
+    public static final String LOG_MARKER = "MainFragmentActivity";
     private Menu toolbarMenu;
+    private Toolbar toolbar;
 
     /**
      * Adds the single fragment, returned from the abstract method createFragment(), into the fragment container.
@@ -38,11 +52,11 @@ public abstract class MainFragmentActivity extends AppCompatActivity implements 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(this.toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, this.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.removeDrawerListener(toggle);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
@@ -66,7 +80,34 @@ public abstract class MainFragmentActivity extends AppCompatActivity implements 
         getMenuInflater().inflate(R.menu.toolbar_options, menu);
         this.toolbarMenu = menu;
 
+        initializeSearchBar(menu);
+
         return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * This method is called when a user selects an item in the menu bar. Opens
+     * the fragment of selected item.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.menu_search:
+            {
+                return true;
+            }
+            case R.id.menu_refresh:
+            {
+                executeRefreshTicker(item);
+                return true;
+            }
+            default:
+            {
+                return super.onOptionsItemSelected(item);
+            }
+        }
     }
 
     @Override
@@ -76,15 +117,15 @@ public abstract class MainFragmentActivity extends AppCompatActivity implements 
         if(drawer.isDrawerOpen(GravityCompat.START))
         {
             drawer.closeDrawer(GravityCompat.START);
-        }else
+        }
+        else
         {
             super.onBackPressed();
         }
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item)
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
     {
         // Handle navigation view item clicks here.
         switch(item.getItemId())
@@ -93,24 +134,28 @@ public abstract class MainFragmentActivity extends AppCompatActivity implements 
             {
                 updateFragmentContainer(new HomeFragment());
                 this.inflateToolbarMenuItems();
+                this.toolbar.setTitle(R.string.nav_home);
                 break;
             }
             case R.id.nav_ticker:
             {
                 updateFragmentContainer(new TickerFragment());
                 this.inflateToolbarMenuItems();
+                this.toolbar.setTitle(R.string.nav_ticker);
                 break;
             }
             case R.id.nav_calculator:
             {
                 updateFragmentContainer(new CalculatorFragment());
                 this.removeToolbarMenuItems();
+                this.toolbar.setTitle(R.string.nav_calculator);
                 break;
             }
             case R.id.nav_settings:
             {
                 updateFragmentContainer(new SettingsFragment());
                 this.removeToolbarMenuItems();
+                this.toolbar.setTitle(R.string.nav_settings);
                 break;
             }
         }
@@ -118,6 +163,18 @@ public abstract class MainFragmentActivity extends AppCompatActivity implements 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void stopRefreshAnimation()
+    {
+        // Get our refresh item from the menu
+        MenuItem refreshMenu = this.toolbarMenu.findItem(R.id.menu_refresh);
+        if(refreshMenu.getActionView() != null)
+        {
+            // Remove the animation.
+            refreshMenu.getActionView().clearAnimation();
+            refreshMenu.setActionView(null);
+        }
     }
 
     protected void updateFragmentContainer(Fragment newFragment)
@@ -140,6 +197,59 @@ public abstract class MainFragmentActivity extends AppCompatActivity implements 
         {
             this.toolbarMenu.clear();
         }
+    }
+
+    protected void initializeSearchBar(Menu menu)
+    {
+        MenuItem myActionMenuItem = menu.findItem(R.id.menu_search);
+        SearchView searchView = (SearchView) myActionMenuItem.getActionView();
+        searchView.setQueryHint(getResources().getString(R.string.search_hint));
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+        {
+            @Override
+            public boolean onQueryTextSubmit(String query)
+            {
+                Log.d(LOG_MARKER, "onQueryTextSubmit: " + query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s)
+            {
+                Log.d(LOG_MARKER, "onQueryTextChange: " + s);
+                return false;
+            }
+        });
+
+        AutoCompleteTextView searchTextView = (AutoCompleteTextView) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        try
+        {
+            Field mCursorDrawableRes = TextView.class.getDeclaredField("mCursorDrawableRes");
+            mCursorDrawableRes.setAccessible(true);
+            mCursorDrawableRes.set(searchTextView, R.drawable.search_cursor); //This sets the cursor resource ID to 0 or @null which will make it visible on white background
+        }
+        catch(Exception e)
+        {
+            Log.e(LOG_MARKER, "Error setting custom search cursor.", e);
+        }
+    }
+
+    protected void executeRefreshTicker(MenuItem item)
+    {
+        // Do animation start
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        ImageView refreshImage = (ImageView) inflater.inflate(R.layout.imageview_refresh, null);
+        Animation rotation = AnimationUtils.loadAnimation(this, R.anim.rotate_refresh);
+        rotation.setRepeatCount(Animation.INFINITE);
+        refreshImage.startAnimation(rotation);
+
+        item.setActionView(refreshImage);
+
+        UpdateTickerTask tickerUpdater = new UpdateTickerTask(this);
+        tickerUpdater.execute();
     }
 
     /**
---
+++ app/src/main/java/com/aaron/pseplanner/async/UpdateTickerTask.java
@@ -0,0 +1,44 @@
+package com.aaron.pseplanner.async;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.aaron.pseplanner.activity.MainFragmentActivity;
+
+/**
+ * Created by Aaron on 11/20/2016.
+ */
+
+public class UpdateTickerTask extends AsyncTask<Void, Void, String>
+{
+    private MainFragmentActivity callerActivity;
+
+    public UpdateTickerTask(MainFragmentActivity callerActivity)
+    {
+        this.callerActivity = callerActivity;
+    }
+
+    @Override
+    protected String doInBackground(Void... params)
+    {
+        try {
+            // Set a time to simulate a long update process.
+            Thread.sleep(2000);
+
+            return null;
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(callerActivity, "Finished complex background function!",
+                Toast.LENGTH_LONG).show();
+
+        callerActivity.stopRefreshAnimation();
+    }
+}
